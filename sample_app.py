af0c077687b3c4956bc00ae4440fec629a41ab47
Yasim <91527193+Yasimcd@users.noreply.github.com>
2023-08-12 06:05:52 +0000

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -1,13 +1,13 @@
 # Add to this file for the sample app lab
 from flask import Flask
 from flask import request
-from flask import render_template
+from flask import render_templates
 
 sample = Flask(__name__)
 
 @sample.route("/")
 def main():
-    return render_template("index.html")
+    return render_templates("index.html")
 
 if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=5050)
